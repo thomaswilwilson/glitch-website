d9e99196d822a30473db80262fb65f453c9dd119
Thomas Wilson <thomaswilson@Thomass-MacBook-Pro-2.local>
2019-05-30 15:07:27 -0500

    plz

--- js/media-payer.js
+++ js/media-payer.js
@@ -40,7 +40,7 @@ function initialiseMediaPlayer() {
 		// Change the button to be a play button
 		changeButtonType(playPauseBtn, 'play');
 		}, false);
-	
+
 	// need to work on this one more...how to know it's muted?
 // 	mediaPlayers[i].addEventListener('volumechange', function() {
 // 		// Update the button to be mute/unmute
@@ -48,6 +48,7 @@ function initialiseMediaPlayer() {
 // 		else changeButtonType(muteBtn, 'mute');
 // 	}, false);
 	mediaPlayers[i].addEventListener('ended', function() { this.pause(); }, false);
+}
 }
 
 function togglePlayPause() {
